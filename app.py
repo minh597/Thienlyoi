562c75bdf09e46952a9cb41cbcf2e358a8df0c96
minh597 <minhdangtran1012@gmail.com>
2025-11-26 16:51:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,6 @@ def test():
     name = request.args.get("name", "bạn")
     return jsonify({"msg": f"Chào {name} từ Thienlyoi API!"})
 
-# Quan trọng: Vercel yêu cầu có hàm này
+# export handler cho Vercel
 def handler(environ, start_response):
     return app(environ, start_response)
-
-if __name__ == "__main__":
-    app.run()
